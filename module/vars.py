cc3659286e90f47352b1179a2b871966647e4bb7
priyachaudhary1128-ctrl <priyachaudhary1128@gmail.com>
2025-10-19 22:36:29 +0530

    Refactor vars.py by removing unnecessary lines

--- module/vars.py
+++ module/vars.py
@@ -4,8 +4,8 @@ import os
 from os import environ
 
 API_ID = int(environ.get("API_ID", "23231900"))
-API_HASH = environ.get("API_HASH", "8212240611:AAErCUazgSQUE2iCq4xbxQjCdxXVUEjikJg")
-BOT_TOKEN = environ.get("BOT_TOKEN", "d030150e323a1ff50851db2044cfe6ad")
+API_HASH = environ.get("API_HASH", "d030150e323a1ff50851db2044cfe6ad")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8339041551:AAG1-JnDrDm-C3iJqzaEma6qYwd7u2nQEEk)
 
 OWNER = int(environ.get("OWNER", "8463673971"))
 CREDIT = environ.get("CREDIT", "BOBBY PIPPAL")
@@ -18,4 +18,5 @@ AUTH_USER = os.environ.get('AUTH_USERS', '8463673971').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
 
