12e203f0f1b223741eb32a75c4e84f9c02a97c26
priyachaudhary1128-ctrl <priyachaudhary1128@gmail.com>
2025-10-19 22:37:42 +0530

    Reformat vars.py with consistent code structure

--- module/vars.py
+++ module/vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "23231900"))
 API_HASH = environ.get("API_HASH", "d030150e323a1ff50851db2044cfe6ad")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8339041551:AAG1-JnDrDm-C3iJqzaEma6qYwd7u2nQEEk)
+BOT_TOKEN = environ.get("BOT_TOKEN", "8339041551:AAG1-JnDrDm-C3iJqzaEma6qYwd7u2nQEEk")
 
 OWNER = int(environ.get("OWNER", "8463673971"))
 CREDIT = environ.get("CREDIT", "BOBBY PIPPAL")
@@ -18,5 +18,6 @@ AUTH_USER = os.environ.get('AUTH_USERS', '8463673971').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
 
 
